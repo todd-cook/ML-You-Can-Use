c606ab202759b691b4e6d030cbf341dc5a6551c5
todd-cook <665389+todd-cook@users.noreply.github.com>
2020-02-24 22:12:38 -0800

    nudge for circleci

--- install_corpora.py
+++ install_corpora.py
@@ -8,7 +8,6 @@ if __name__ == '__main__':
     LOG = logging.getLogger(__name__)
     LOG.addHandler(logging.NullHandler())
     logging.basicConfig(level=logging.INFO)
-
     try:
         corpus_importer = CorpusImporter('latin')
         corpus_importer.import_corpus('latin_text_latin_library')
@@ -17,5 +16,11 @@ if __name__ == '__main__':
         corpus_importer = CorpusImporter('greek')
         corpus_importer.import_corpus('greek_text_perseus')
         corpus_importer.import_corpus('greek_text_lacus_curtius')
+        from transformers import BertTokenizer, BertForMaskedLM, GPT2Tokenizer, GPT2LMHeadModel
+        bert_tokenizer = BertTokenizer.from_pretrained('bert-large-cased')
+        bert_model = BertForMaskedLM.from_pretrained("bert-large-cased-whole-word-masking")
+        gpt2_tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+        gpt2_model = GPT2LMHeadModel.from_pretrained('gpt2')
+        logging.disable(logging.NOTSET)
     except:
         LOG.exception('Failure to download test corpora')
