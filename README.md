8e00a86fc3f1ffe3e4930b891b8ef21e569a8151
todd-cook <665389+todd-cook@users.noreply.github.com>
2020-04-19 22:10:08 -0700

    adding BERT & Cleanlab example

--- README.md
+++ README.md
@@ -46,40 +46,48 @@ mkdir p3
 ### Interactivity
 ``juypter notebook`` 
 
-## Recommended Sequence
+## Notebooks
+
+### Getting data
+* [Extracting Occupation and Employer data from Wikidata](getting_data/extracting_occupation_and_employer_data_from_wikidata.ipynb) 
+
+### Labeling Data
+* [Labeling occupation data with Wikipedia and GoogleNews](labeling_data/labeling_occupation_data_with_Wikipedia_and_GoogleNews.ipynb)
+* [Correcting GoogleNews labels with Cleanlab](labeling_data/correcting_GoogleNews_labels_with_Cleanlab.ipynb)
+* [Training to label with BERT and Cleanlab](labeling_data/training_to_label_with_BERT_and_Cleanlab.ipynb)
 
 ### Modeling Language
-* [1. Assessing Corpus Quality](probablistic_language_modeling/assessing_corpus_quality.ipynb)
-* [2. Making a Frequency Distribution](probablistic_language_modeling/make_frequency_distribution.ipynb)
-* [3. Making a Word Trie Probability Model](probablistic_language_modeling/make_trie_language_model.ipynb)
-* [4. Word and Sentence Probability using BERT](probablistic_language_modeling/automatic_grammatical_error_corrections_using_BERT_GPT2.ipynb)
+* [Assessing Corpus Quality](probablistic_language_modeling/assessing_corpus_quality.ipynb)
+* [Making a Frequency Distribution](probablistic_language_modeling/make_frequency_distribution.ipynb)
+* [Making a Word Trie Probability Model](probablistic_language_modeling/make_trie_language_model.ipynb)
+* [Word and Sentence Probability using BERT](probablistic_language_modeling/automatic_grammatical_error_corrections_using_BERT_GPT2.ipynb)
 ### Detecting Duplicate Documents
-* [5. Merge corpora by detecting and filtering duplicate documents](document_deduplication/find_duplicate_docs.ipynb)
+* [Merge corpora by detecting and filtering duplicate documents](document_deduplication/find_duplicate_docs.ipynb)
 ### Classifying Texts
-* [6. Benchmarking our classifier](text_classification/imdb_benchmark.ipynb)
-* [7. Boostrapping Document Classification](text_classification/bootstrapping_document_classification.ipynb)
+* [Benchmarking our classifier](text_classification/imdb_benchmark.ipynb)
+* [Boostrapping Document Classification](text_classification/bootstrapping_document_classification.ipynb)
 ### Detecting Loanwords
-* [8. Making a Frequency Distribution of Transliterated Greek](detecting_loanwords/make_frequency_distribution_greek_transliterated.ipynb)
-* [9. Boosting Training Data](detecting_loanwords/boosting_training_data.ipynb)
-* [10. The Problem of Loanwords, and a Solution](detecting_loanwords/loanwords_problems_solutions.ipynb)
-* [11. Feature Engineering with the Loanwords matrix](detecting_loanwords/loanwords_feature_engineering.ipynb)
-* [12. Detecting Loanwords with Keras](detecting_loanwords/detecting_loanwords_keras..ipynb)
+* [Making a Frequency Distribution of Transliterated Greek](detecting_loanwords/make_frequency_distribution_greek_transliterated.ipynb)
+* [Boosting Training Data](detecting_loanwords/boosting_training_data.ipynb)
+* [The Problem of Loanwords, and a Solution](detecting_loanwords/loanwords_problems_solutions.ipynb)
+* [Feature Engineering with the Loanwords matrix](detecting_loanwords/loanwords_feature_engineering.ipynb)
+* [Detecting Loanwords with Keras](detecting_loanwords/detecting_loanwords_keras.ipynb)
 ### Wikipedia Corpus Processing
-* [13. English Wikipedia Corpus Cleaning](wikipedia_corpus_processing/clean_english_wiki_corpus.ipynb)
-* [14. English Wikipedia Corpus Processing](wikipedia_corpus_processing/create_corpus_from_english_wiki.ipynb)
-* [15. Latin Corpus Processing](wikipedia_corpus_processing/create_corpus_from_latin_wiki.ipynb)
-* [16. Down sample or not](wikipedia_corpus_processing/down_sample_or_not.ipynb)
+* [English Wikipedia Corpus Cleaning](wikipedia_corpus_processing/clean_english_wiki_corpus.ipynb)
+* [English Wikipedia Corpus Processing](wikipedia_corpus_processing/create_corpus_from_english_wiki.ipynb)
+* [Latin Corpus Processing](wikipedia_corpus_processing/create_corpus_from_latin_wiki.ipynb)
+* [Downsample or not](wikipedia_corpus_processing/down_sample_or_not.ipynb)
 ### Quality Embeddings 
-* [17. Generate English Wikipedia word vector](quality_embeddings/generate_latin_word_vector.ipynb) 
-* [18. Generate Latin word vector](quality_embeddings/generate_latin_word_vector.ipynb)
-* [19. The Case for Using an Embedding Encoder](quality_embeddings/embedding_encoder.ipynb) 
+* [Generating an English Wikipedia word vector](quality_embeddings/generate_latin_word_vector.ipynb) 
+* [Generating a Latin word vector](quality_embeddings/generate_latin_word_vector.ipynb)
+* [The Case for Using an Embedding Encoder](quality_embeddings/embedding_encoder.ipynb) 
 ### Computer Vision - Object Detection
-* [20. Object detection as a multivariable regression using a custom Convnet](computer_vision_object_detection/train_noisy_circle_detector.ipynb)
-* [21. Assessing the Noisy Circle detector](computer_vision_object_detection/assess_noisy_circle_detector.ipynb)
+* [Object detection as a multivariable regression using a custom Convnet](computer_vision_object_detection/train_noisy_circle_detector.ipynb)
+* [Assessing the Noisy Circle detector](computer_vision_object_detection/assess_noisy_circle_detector.ipynb)
 ### Summarizing Texts
-* [22. Assessing Headline Generation](summarization/neural_headline_generation_metrics.ipynb)
+* [Assessing Headline Generation](summarization/neural_headline_generation_metrics.ipynb)
 
-# References and Acknowledgements    
+#### References and Acknowledgements    
 * [A Sensitivity Analysis of (and Practitioners' Guide to) Convolutional Neural Networks for Sentence Classification by Ye Zhang, Byron Wallace](https://arxiv.org/abs/1510.03820)
 * [Word2vec applied to Recommendation: Hyperparameters Matter byHugo Caselles-Dupré, Florian Lesaint, Jimena Royo-Letelier](https://arxiv.org/pdf/1804.04212)
 * [Exploiting Similarities among Languages for Machine Translation by Tomas Mikolov, Quoc V. Le, Ilya Sutskever](https://arxiv.org/abs/1309.4168)
---
+++ mlyoucanuse/language_code_utils.py
@@ -0,0 +1,335 @@
+"""`language_code_utils.py` - utilty methods for dealing with fasttext language codes.
+Some of the less common language code mappings were found in the iso639 library:
+https://github.com/noumar/iso639
+"""
+import logging
+from typing import Any, Tuple
+
+from numpy import array
+
+LOG = logging.getLogger(__name__)
+LOG.addHandler(logging.NullHandler())
+
+
+def fast_text_prediction_to_three_letter_language_code(
+    res: Tuple[Tuple[str, ...], Tuple[array]]
+) -> Any:
+    """Get a two letter language code from a fasttext language detection model.
+
+    >>> import numpy as np
+    >>> res = (('__label__eng', '__label__ron', '__label__vie'),
+    ... np.asarray([9.99590933e-01, 2.40980095e-04, 1.49953354e-04])
+    ... ) # or res = mdl.predict('The quick brown fox', k=3)
+    >>> fast_text_prediction_to_three_letter_language_code(res)
+    ['eng', 'ron', 'vie']
+    >>> res = (('__label__eng',), np.array([0.99959093])
+    ... ) # or res = mdl.predict('The quick brown fox')
+    >>> fast_text_prediction_to_three_letter_language_code(res)
+    'eng'
+
+    """
+    if len(res[0]) == 1:
+        label, _ = res
+        return label[0][-3:]
+    labels, _ = res
+    return [tmp[-3:] for tmp in labels]
+
+
+def fast_text_prediction_to_two_letter_language_code(
+    res: Tuple[Tuple[str, ...], Tuple[array]]
+) -> Any:
+    """Get a two letter language code from a fasttext language detection model.
+
+    >>> import numpy as np
+    >>> res = (('__label__eng', '__label__ron', '__label__vie'),
+    ... np.asarray([9.99590933e-01, 2.40980095e-04, 1.49953354e-04])
+    ... ) # or res = mdl.predict('The quick brown fox', k=3)
+    >>> fast_text_prediction_to_two_letter_language_code(res)
+    ['en', 'ro', 'vi']
+    >>> res = (('__label__eng',), np.array([0.99959093])
+    ... ) # or res = mdl.predict('The quick brown fox')
+    >>> fast_text_prediction_to_two_letter_language_code(res)
+    'en'
+
+    """
+    if len(res[0]) == 1:
+        label, _ = res
+        return _get_name(label[0])
+    labels, _ = res
+    return [_get_name(tmp) for tmp in labels]
+
+
+def _get_name(label):
+    val = THREE_LETTER_TO_TWO_LETTER_LANGUAGE_CODES.get(label[-3:])
+    if not val:
+        LOG.warning("Language not found %s", label)
+        return "Unknown"
+    return val
+
+
+THREE_LETTER_TO_TWO_LETTER_LANGUAGE_CODES = {
+    "abk": "ab",  #: Abkhazian
+    "aar": "aa",  #: Afar
+    "afr": "af",  #: Afrikaans
+    "alb": "sq",  #: Albanian
+    "sqi": "sq",  #: Albanian
+    "amh": "am",  #: Amharic
+    "ara": "ar",  #: Arabic
+    "arg": "an",  #: Aragonese
+    "arm": "hy",  #: Armenian
+    "hye": "hy",  #: Armenian
+    "asm": "as",  #: Assamese
+    "ave": "ae",  #: Avestan
+    "aym": "ay",  #: Aymara
+    "aze": "az",  #: Azerbaijani
+    "bak": "ba",  #: Bashkir
+    "baq": "eu",  #: Basque
+    "eus": "eu",  #: Basque
+    "bel": "be",  #: Belarusian
+    "ben": "bn",  #: Bengali
+    "bih": "bh",  #: Bihari
+    "bis": "bi",  #: Bislama
+    "bos": "bs",  #: Bosnian
+    "bre": "br",  #: Breton
+    "bul": "bg",  #: Bulgarian
+    "bur": "my",  #: Burmese
+    "mya": "my",  #: Burmese
+    "cat": "ca",  #: Catalan
+    "cha": "ch",  #: Chamorro
+    "che": "ce",  #: Chechen
+    "chi": "zh",  #: Chinese
+    "zho": "zh",  #: Chinese
+    "chu": "cu",  #: Church Slavic; Slavonic; Old Bulgarian
+    "chv": "cv",  #: Chuvash
+    "cor": "kw",  #: Cornish
+    "cos": "co",  #: Corsican
+    "scr": "hr",  #: Croatian
+    "hrv": "hr",  #: Croatian
+    "cze": "cs",  #: Czech
+    "ces": "cs",  #: Czech
+    "dan": "da",  #: Danish
+    "div": "dv",  #: Dhivehi; Maldivian
+    "dut": "nl",  #: Dutch
+    "nld": "nl",  #: Dutch
+    "dzo": "dz",  #: Dzongkha
+    "eng": "en",  #: English
+    "epo": "eo",  #: Esperanto
+    "est": "et",  #: Estonian
+    "fao": "fo",  #: Faroese
+    "fij": "fj",  #: Fijian
+    "fin": "fi",  #: Finnish
+    "fre": "fr",  #: French
+    "fra": "fr",  #: French
+    "gla": "gd",  #: 'Gaelic': 'Scottish',  #:  Gaelic;
+    "glg": "gl",  #: Galician
+    "geo": "ka",  #: Georgian
+    "kat": "ka",  #: Georgian
+    "ger": "de",  #: German
+    "deu": "de",  #: German
+    "gre": "el",  #: Greek, Modern (1453-)
+    "ell": "el",  #: Greek, Modern (1453-)
+    "grn": "gn",  #: Guarani
+    "guj": "gu",  #: Gujarati
+    "hat": "ht",  #: Haitian; Haitian Creole
+    "ina": "ia",
+    #: Interlingua (International Auxiliary Language Association)
+    "hau": "ha",  #: Hausa
+    "heb": "he",  #: Hebrew
+    "her": "hz",  #: Herero
+    "hin": "hi",  #: Hindi
+    "ho": "Motu",  #: Hiri
+    "hun": "hu",  #: Hungarian
+    "ice": "is",  #: Icelandic
+    "isl": "is",  #: Icelandic
+    "ido": "io",  #: Ido
+    "ind": "id",  #: Indonesian
+    #: 'Auxiliary': '(International',  #:  Interlingua
+    "ile": "ie",  #: Interlingue
+    "iku": "iu",  #: Inuktitut
+    "ipk": "ik",  #: Inupiaq
+    "gle": "ga",  #: Irish
+    "ita": "it",  #: Italian
+    "jpn": "ja",  #: Japanese
+    "jav": "jv",  #: Javanese
+    "kal": "kl",  #: Kalaallisut
+    "kan": "kn",  #: Kannada
+    "kas": "ks",  #: Kashmiri
+    "kaz": "kk",  #: Kazakh
+    "khm": "km",  #: Khmer
+    "kik": "ki",  #: Kikuyu; Gikuyu
+    "kin": "rw",  #: Kinyarwanda
+    "kir": "ky",  #: Kirghiz
+    "kom": "kv",  #: Komi
+    "kor": "ko",  #: Korean
+    "kua": "kj",  #: Kuanyama; Kwanyama
+    "kur": "ku",  #: Kurdish
+    "lao": "lo",  #: Lao
+    "lat": "la",  #: Latin
+    "lav": "lv",  #: Latvian
+    "lim": "li",
+    #: 'Limburgish': 'Limburger;',  #:  Limburgan;
+    "lin": "ln",  #: Lingala
+    "lit": "lt",  #: Lithuanian
+    "ltz": "lb",  #: Luxembourgish; Letzeburgesch
+    "mac": "mk",  #: Macedonian
+    "mkd": "mk",  #: Macedonian
+    "mlg": "mg",  #: Malagasy
+    "may": "ms",  #: Malay
+    "msa": "ms",  #: Malay
+    "mal": "ml",  #: Malayalam
+    "mlt": "mt",  #: Maltese
+    "glv": "gv",  #: Manx
+    "mao": "mi",  #: Maori
+    "mri": "mi",  #: Maori
+    "mar": "mr",  #: Marathi
+    "mah": "mh",  #: Marshallese
+    "mol": "mo",  #: Moldavian
+    "mon": "mn",  #: Mongolian
+    "nau": "na",  #: Nauru
+    "nav": "nv",  #: Navajo ,  Navaho,
+    "nde": "nd",  #: North Ndebele
+    "nbl": "nr",  #: South Ndebele
+    "ndo": "ng",  #: Ndonga
+    "nep": "ne",  #: Nepali
+    "sme": "se",  #: Northern Sami
+    "nor": "no",  #: Norwegian
+    "nob": "nb",  #: Norwegian Bokmal
+    "nno": "nn",  #: Norwegian Nynorsk
+    "nya": "ny",  #: 'Chewa': 'Chichewa;',  #:  Nyanja;
+    "oci": "oc",  #: '1500);': '(post',  #:  Occitan
+    "ori": "or",  #: Oriya
+    "orm": "om",  #: Oromo
+    "oss": "os",  #: Ossetian; Ossetic
+    "sot": "st",  #: Sotho, Southern	st	sot
+    "spa": "es",  #: Spanish; Castilian	es	spa
+    "fry": "fy",  #: Western Frisian	fy	fry
+    "zha": "za",  #: Zhuang; Chuang	za	zha
+    "pli": "pi",  #: Pali
+    "pan": "pa",  #: Panjabi
+    "per": "fa",  #: Persian
+    "fas": "fa",  #: Persian
+    "pol": "pl",  #: Polish
+    "por": "pt",  #: Portuguese
+    "pus": "ps",  #: Pushto
+    "que": "qu",  #: Quechua
+    "roh": "rm",  #: Raeto-Romance
+    "rum": "ro",  #: Romanian
+    "ron": "ro",  #: Romanian
+    "run": "rn",  #: Rundi
+    "rus": "ru",  #: Russian
+    "smo": "sm",  #: Samoan
+    "sag": "sg",  #: Sango
+    "san": "sa",  #: Sanskrit
+    "srd": "sc",  #: Sardinian
+    "scc": "sr",  #: Serbian
+    "srp": "sr",  #: Serbian
+    "sna": "sn",  #: Shona
+    "iii": "ii",  #: Sichuan Yi
+    "snd": "sd",  #: Sindhi
+    "sin": "si",  #: Sinhala; Sinhalese
+    "slo": "sk",  #: Slovak
+    "slk": "sk",  #: Slovak
+    "slv": "sl",  #: Slovenian
+    "som": "so",  #: Somali
+    "sun": "su",  #: Sundanese
+    "swa": "sw",  #: Swahili
+    "ssw": "ss",  #: Swati
+    "swe": "sv",  #: Swedish
+    "tgl": "tl",  #: Tagalog
+    "tah": "ty",  #: Tahitian
+    "tgk": "tg",  #: Tajik
+    "tam": "ta",  #: Tamil
+    "tat": "tt",  #: Tatar
+    "tel": "te",  #: Telugu
+    "tha": "th",  #: Thai
+    "tib": "bo",  #: Tibetan
+    "bod": "bo",  #: Tibetan
+    "tir": "ti",  #: Tigrinya
+    "ton": "to",  #: 'Islands)': '(Tonga',  #:  Tonga
+    "tso": "ts",  #: Tsonga
+    "tsn": "tn",  #: Tswana
+    "tur": "tr",  #: Turkish
+    "tuk": "tk",  #: Turkmen
+    "twi": "tw",  #: Twi
+    "uig": "ug",  #: Uighur
+    "ukr": "uk",  #: Ukrainian
+    "urd": "ur",  #: Urdu
+    "uzb": "uz",  #: Uzbek
+    "vie": "vi",  #: Vietnamese
+    "vol": "vo",  #: Volapuk
+    "wln": "wa",  #: Walloon
+    "wel": "cy",  #: Welsh
+    "cym": "cy",  #: Welsh
+    "wol": "wo",  #: Wolof
+    "xho": "xh",  #: Xhosa
+    "yid": "yi",  #: Yiddish
+    "yor": "yo",  #: Yoruba
+    "zul": "zu",  #: Zulu
+    # The following entries are less common:
+    # pulled from the iso-639 library; they don't have two character language codes
+    "ang": "Old English (ca. 450-1100)",
+    "arq": "Algerian Arabic",
+    "arz": "Egyptian Arabic",
+    "avk": "Kotava",
+    "bar": "Bavarian",
+    "cbk": "Chavacano",
+    "ceb": "Cebuano",
+    "cho": "Choctaw",
+    "cmn": "Mandarin Chinese",
+    "crh": "Crimean Tatar",
+    "csb": "Kashubian",
+    "dsb": "Lower Sorbian",
+    "dtp": "Central Dusun",
+    "egl": "Emilian",
+    "fkv": "Kven Finnish",
+    "gcf": "Guadeloupean Creole French",
+    "gos": "Gronings",
+    "got": "Gothic",
+    "grc": "Ancient Greek (to 1453)",
+    "haw": "Hawaiian",
+    "hoc": "Ho",
+    "hrx": "Hunsrik",
+    "hsb": "Upper Sorbian",
+    "ilo": "Iloko",
+    "jbo": "Lojban",
+    "kab": "Kabyle",
+    "kha": "Khasi",
+    "krl": "Karelian",
+    "kzj": "Coastal Kadazan",
+    "lad": "Ladino",
+    "ldn": "Láadan",
+    "lfn": "Lingua Franca Nova",
+    "lij": "Ligurian",
+    "lvs": "Standard Latvian",
+    "lzh": "Literary Chinese",
+    "mhr": "Eastern Mari",
+    "min": "Minangkabau",
+    "mrj": "Western Mari",
+    "nds": "Low German",
+    "npi": "Nepali (individual language)",
+    "nst": "Tase Naga",
+    "ota": "Ottoman Turkish (1500-1928)",
+    "pam": "Pampanga",
+    "pcd": "Picard",
+    "pes": "Iranian Persian",
+    "pms": "Piemontese",
+    "prg": "Prussian",
+    "rif": "Tarifit",
+    "rom": "Romany",
+    "sah": "Yakut",
+    "shs": "Shuswap",
+    "shy": "Tachawit",
+    "sux": "Sumerian",
+    "swg": "Swabian",
+    "swh": "Swahili (individual language)",
+    "thv": "Tahaggart Tamahaq",
+    "tlh": "Klingon",
+    "tpi": "Tok Pisin",
+    "tpw": "Tupí",
+    "tzl": "Talossan",
+    "war": "Waray (Philippines)",
+    "wuu": "Wu Chinese",
+    "zlm": "Malay (individual language)",
+    "zsm": "Standard Malay",
+    "zza": "Zaza",
+}
